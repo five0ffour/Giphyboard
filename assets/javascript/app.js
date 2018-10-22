2da42c81e3914e9f6ef8a1e7dcf579c24c51c19f
Mike Galarneau <mbgalarneau@gmail.com>
2018-10-22 19:52:30 -0400

    fully functional code to homework specs, needs testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,13 +4,76 @@
 $(document).ready(function () {
 
     // GLOBAL VARIABLES 
-    var searches = []; //  hold the metadata about each giphy submission
-    var cartoonItem = {
+    var topics = [
+        "inside out",
+        "WALL-E",
+        "toy story",
+        "finding nemo",
+        "the incredibles",
+        "ratatouille",
+        "up",
+        "monsters, inc.",
+        "pixar cars",
+        "a bug's life",
+        "pixar brave",
+        "disney shrek",
+        "aladdin",
+        "the lion king",
+    ];
+    var gifItem = {
+        lastSearch: "", // topic of this query
         gifMetaData: [], // array of query data objets
         gifImg: [], // array of <img> objects
         animated: false // display the static image as the default
     }
 
+    /******************************************* */
+    /* initalize the page with our seeded topics */
+    /******************************************* */
+    loadPage();
+
+    //-----------------------------
+    // loadPage() - runs as soon as the page is refreshed (loaded), it creates the default buttons of our topic    
+    //-----------------------------
+    function loadPage() {
+
+        // Loop through each of the topics adding a button of that name
+        for (var i = 0;
+            (i < topics.length); i++) {
+            var title = topics[i];
+            addButton(title);
+        }
+        performGiphySearch(topics[0]);
+    }
+
+    // addButton() - adds a button to the title
+    function addButton(title) {
+
+        // Add a button to the main title screen
+        var btn = $("<button>");
+        btn.text(title);
+        btn.addClass("gif-btn");
+        btn.attr("data-topic", title);
+        $("#gifButtons").append(btn);
+    }
+
+    //---------------------------
+    // performGiphySearch(title) - uses the passed parameter to run a query against the Giphy back end API
+    //-----------------------------
+    function performGiphySearch(title) {
+
+        // Build the query URL for the ajax request to the NYT API
+        var queryURL = buildQueryURL(title);
+
+        gifItem.lastSearch = title;
+
+        // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+        // The data then gets passed as an argument to the updatePage function
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(updatePage);
+    }
 
     //---------------------------
     // buildQueryURL() - utility function to build the API query based on a title parameter
@@ -40,57 +103,60 @@ $(document).ready(function () {
     }
 
     //---------------------------
-    // updatePage(giphyData) - called by AJAX resposne, turns JSON  into elements on the page
+    // updatePage(responseData) - called by AJAX resposne, turns JSON  into elements on the page
     //---------------------------
-    function updatePage(giphyData) {
+    function updatePage(responseData) {
 
         // Cap the number of gifs to display at 10 images
-        const numGifs = giphyData.data.length < 10 ? giphyData.data.length : 10;
-
-        // save the button search meta data so we can recall it later
-        searches.push(cartoonItem);
+        const numGifs = responseData.data.length < 10 ? responseData.data.length : 10;
 
         // Log the giphyData to console, where it will show up as an object
-        console.log(giphyData);
+        console.log(responseData);
         console.log("------------------------------------");
 
         // Loop through and build buttons for the defined number of gifs
         for (var i = 0; i < numGifs; i++) {
+
             // Get specific gif metadata for current index
-            var gif = giphyData.data[i];
+            var gifData = responseData.data[i];
+            var gifListElem = $("<ul>");
+            var gifListItemElem = $("<li class='list-item'>");
 
-            // Create the  list group to contain the articles and add the article content for each
-            var gifList = $("<ul>");
-            gifList.addClass("list-group");
+            // Add Title
+            var gifSpanTitle = $("<span>");
+            gifSpanTitle.addClass("label");
+            gifSpanTitle.html("<strong>" + gifData.title + "</strong>");
+            gifListItemElem.append(gifSpanTitle);
 
-            // Add the newly created element to the DOM
-            $("#cartoons").append(gifList);
+            // Add Rating
+            var gifSpanRating = $("<span>");
+            gifListItemElem.append($("<br>"));
+            gifSpanRating.html("Rating: " + gifData.rating);
+            gifListItemElem.append(gifSpanRating);
+            gifListItemElem.append($("<br>"));
 
-            var gifListItem = $("<li class='list-item'>");
+            // Add CSS groups to List Elements and attach to main HTML element 
+            gifListElem.addClass("list-group");
+            $("#gifs").append(gifListElem);
 
-            gifListItem.append(
-                "<span class='label'>" +
-                "<strong>" + gif.title + "</strong>" +
-                "</span>" +
-                " - " +
-                "<span> Rating: " + gif.rating + "</span>" +
-                "<br></br>"
-            );
-
-            // Create a new <img> element
-            var gifImg = $("<img>");
+            // Find the index of the current item and save it as metadata to the element
+            var searchIdx = topics.indexOf(gifItem.lastSearch);
 
             // Save the metadata and link it to the DOM elements  for use by the image click event
-            cartoonItem.gifMetaData.push(gif);
+            gifItem.gifMetaData.push(gifData);
 
             // Update the screen item, with indexes it to the saved metadata
-            $(gifImg).addClass("cartoon-img");
-            $(gifImg).attr("src", gif.images.fixed_height_still.url);
-            $(gifImg).attr("alt", gif.title);
-            $(gifImg).attr("data-searchIdx", searches.length - 1);
-            $(gifImg).attr("data-gifIdx", cartoonItem.gifMetaData.length - 1);
-            gifListItem.append(gifImg);
-            gifList.append(gifListItem);
+            // Create a new <img> element
+            var gifImgElem = $("<img>");
+            $(gifImgElem).addClass("gif-img");
+            $(gifImgElem).attr("src", gifData.images.fixed_height_still.url);
+            $(gifImgElem).attr("alt", gifData.title);
+            $(gifImgElem).attr("data-topicIdx", searchIdx);
+            $(gifImgElem).attr("data-gifIdx", gifItem.gifMetaData.length - 1);
+
+            // Chain it all to the DOM
+            gifListItemElem.append(gifImgElem);
+            gifListElem.append(gifListItemElem);
         }
     }
 
@@ -99,7 +165,7 @@ $(document).ready(function () {
     // clear() -  empty out the images on the screen in advance of a refresh
     //-----------------------
     function clear() {
-        $("#cartoons").empty();
+        $("#gifs").empty();
     }
 
     // ==========================================================
@@ -107,11 +173,19 @@ $(document).ready(function () {
     // ==========================================================
 
     //-----------------------
-    // $("#addCartoon").on("click") - event handler for "Add Button" event - adds a new button and associated images to the screen
+    // $(document).on("click", ".gif-btn") - generatic function to fresh screen on query button clicks
     //-----------------------
-    $("#addCartoon").on("click", function (event) {
+    $(document).on("click", ".gif-btn", function () {
+        clear();
+        performGiphySearch($(this).attr("data-topic"));
+    });
+
+    //-----------------------
+    // $("#addGif").on("click") - event handler for "Add Button" event - adds a new button and associated images to the screen
+    //-----------------------
+    $("#addGif").on("click", function (event) {
         // This line allows us to take advantage of the HTML "submit" property
-        // This way we can hit enter on the keyboard and it registers the search
+        // This way we can hit enter on the keyboard and it registers the topic
         // (in addition to clicks). Prevents the page from reloading on form submit.
         event.preventDefault();
 
@@ -119,25 +193,9 @@ $(document).ready(function () {
         clear();
 
         // Grab text the user typed into the search input and build a query string around it
-        var cartoonTitle = $("#cartoon-input")
-            .val()
-            .trim();
-
-        // Build the query URL for the ajax request to the NYT API
-        var queryURL = buildQueryURL(cartoonTitle);
-
-        // Add a button to the main title screen
-        var btn = $("<button>");
-        btn.text(cartoonTitle);
-        btn.addClass("cartoon-btn");
-        $("#cartoonButtons").append(btn);
-
-        // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-        // The data then gets passed as an argument to the updatePage function
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(updatePage);
+        var title = $("#gif-input").val().trim();
+        addButton(title);
+        performGiphySearch(title);
     });
 
     //-----------------------
@@ -146,27 +204,19 @@ $(document).ready(function () {
     $("#clear-all").on("click", clear);
 
     //-----------------------
-    // $(document).on("click", ".cartoon-btn") - generatic function to fresh screen on query button clicks
+    // $(document).on("click",".gif-img")  - generic function to animate specific image on any image click
     //-----------------------
-    $(document).on("click", ".cartoon-btn", function () {
-        alert("clicked button!");
-    });
+    $(document).on("click", ".gif-img", function () {
 
-    //-----------------------
-    // $(document).on("click",".cartoon-img")  - generic function to animate specific image on any image click
-    //-----------------------
-    $(document).on("click", ".cartoon-img", function () {
 
-        var cartoonItem = searches[$(this).attr("data-searchIdx")];
-        var gifMeta = cartoonItem.gifMetaData[$(this).attr("data-gifIdx")];
-        if (cartoonItem) {
-            cartoonItem.animated = !cartoonItem.animated;
-            if (cartoonItem.animated) {
+        var gifMeta = gifItem.gifMetaData[$(this).attr("data-gifIdx")];
+        if (gifItem) {
+            gifItem.animated = !gifItem.animated;
+            if (gifItem.animated) {
                 $(this).attr("src", gifMeta.images.fixed_height.url);
             } else {
                 $(this).attr("src", gifMeta.images.fixed_height_still.url);
             }
         }
     });
-
 });
